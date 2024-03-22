dbdf1a0fc780b2a5a246b822ab2cc54e66b0bf52
Hazzu <hailaconsutu558@gmail.com>
2024-03-22 12:25:34 +0700

    Improve time and memory model

--- ThresholdModel.py
+++ ThresholdModel.py
@@ -1,61 +1,61 @@
-import os
-import cv2
+import os, cv2
+from re import S
 import numpy as np
 
 class ThresholdModel:  
     def __init__(self, images_path: str, annotations_path: str) -> None:
         self.label_colors = dict()
         self.threshold = dict()
+        self.__statistic = dict()
         self.images_path = images_path
         self.annotations_path = annotations_path
     
-    def __handle_image(self, image_name: str, annotation_name: str, label_name: str):
-        image = cv2.imread(os.path.join(self.images_path, image_name))
-        annotation = cv2.imread(os.path.join(self.annotations_path, annotation_name))
-        
-        intensity = image[np.where(np.all(annotation == self.label_colors[label_name], axis=2))]
-        return (intensity[:, 0], intensity[:, 1], intensity[:, 2])
-        
-    def add_label(self, label_name: str, label_color: list) -> None:
+    def add_label(self, label_name: str, label_color: list[3]) -> None:
         self.label_colors[label_name] = label_color
+        
         self.threshold[label_name] = {
-            'min': [0, 0, 0],
-            'max': [0, 0, 0]
+            'min': np.array([0, 0, 0], dtype=np.float64),
+            'max': np.array([0, 0, 0], dtype=np.float64)
         }
-    
+        
+        self.__statistic[label_name] = {
+            'size': np.array([0, 0, 0], dtype=np.float64),
+            'sum': np.array([0, 0, 0], dtype=np.float64),
+            'dev_square': np.array([0, 0, 0], dtype=np.float64),
+            'mean': np.array([0, 0, 0], dtype=np.float64),
+            'std': np.array([0, 0, 0], dtype=np.float64)
+        }
+        
     def fit(self, images_name: list[str], annotations_name: list[str]) -> None:
-        for label in self.label_colors.keys():
-            B_sum, G_sum, R_sum = 0, 0, 0
-            B_cnt, G_cnt, R_cnt = 0, 0, 0
+        for image_name, annotation_name in zip(images_name, annotations_name):
+            image = cv2.imread(os.path.join(self.images_path, image_name))
+            annotation = cv2.imread(os.path.join(self.annotations_path, annotation_name))
             
-            for image, annotation in zip(images_name, annotations_name):
-                res = self.__handle_image(image, annotation, label)
+            for label_name in self.label_colors.keys():
+                pixels = image[np.where(np.all(annotation == self.label_colors[label_name], axis=2))]
+                self.__statistic[label_name]['sum'] += pixels.sum(axis=0)
+                self.__statistic[label_name]['size'] += np.sum(pixels > 0, axis=0)
+            
+        for key, item in self.__statistic.items():
+            item['mean'] = item['sum'] / item['size']
                 
-                B_sum += np.sum(res[0]); B_cnt += np.size(res[0])
-                G_sum += np.sum(res[1]); G_cnt += np.size(res[1])
-                R_sum += np.sum(res[2]); R_cnt += np.size(res[2])
-                
-            B_mean = B_sum / B_cnt
-            G_mean = G_sum / G_cnt
-            R_mean = R_sum / R_cnt
+        for image_name, annotation_name in zip(images_name, annotations_name):
+            image = cv2.imread(os.path.join(self.images_path, image_name))
+            annotation = cv2.imread(os.path.join(self.annotations_path, annotation_name))
             
-            B_sum_square, G_sum_square, R_sum_square = 0, 0, 0
+            for label_name in self.label_colors.keys():
+                item = self.__statistic[label_name]
+                pixels = image[np.where(np.all(annotation == self.label_colors[label_name], axis=2))]
+                item['dev_square'] += np.sum((pixels - item['mean']) ** 2, axis=0)
+        
+        for label_name in self.label_colors.keys():
+            item = self.__statistic[label_name]
+            item['std'] = np.sqrt(item['dev_square'] / item['size'])
             
-            for image, annotation in zip(images_name, annotations_name):
-                res = self.__handle_image(image, annotation, label)
-                
-                B_sum_square += np.sum((res[0] - B_mean) ** 2)
-                G_sum_square += np.sum((res[1] - G_mean) ** 2)
-                R_sum_square += np.sum((res[2] - R_mean) ** 2)
-
-            B_stddev = np.sqrt(B_sum_square / B_cnt)
-            G_stddev = np.sqrt(G_sum_square / G_cnt)
-            R_stddev = np.sqrt(R_sum_square / R_cnt)
+            self.threshold[label_name]['min'] = item['mean'] - item['std']
+            self.threshold[label_name]['max'] = item['mean'] + item['std']
             
-            self.threshold[label]['min'] = [B_mean - B_stddev, G_mean - G_stddev, R_mean - R_stddev]
-            self.threshold[label]['max'] = [B_mean + B_stddev, G_mean + G_stddev, R_mean + R_stddev]
-            
-            print(label, self.threshold[label]['min'], self.threshold[label]['max'])
+            print(label_name, self.threshold[label_name]['min'], self.threshold[label_name]['max'])
 
     def predict(self, image: np.ndarray, label: str) -> np.ndarray:
         mask = image.copy()
@@ -63,3 +63,16 @@ class ThresholdModel:
         mask[np.where(pos)] = self.label_colors[label]
         mask[np.where(np.logical_not(pos))] = [0, 0, 0]
         return mask
+    
+    def get_mask(self, mask: np.ndarray, label_name: str) -> np.ndarray:
+        mask_label = mask.copy()
+        mask_label[np.where(np.all(mask_label != self.label_colors[label_name], axis=2))] = [0, 0, 0]
+        return mask_label
+    
+# def pixel_accuracy(predict: np.ndarray, mask: np.ndarray):
+    # return np.sum(predict == mask) / np.size(mask)
+    
+def iou_measure(predict: np.ndarray, mask: np.ndarray):
+    intersect = np.sum(np.all((predict & mask) != [0, 0, 0], axis=2))
+    union = np.sum(np.all((predict | mask) != [0, 0, 0], axis=2))
+    return intersect / union
